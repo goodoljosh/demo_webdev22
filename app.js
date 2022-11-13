722a33e55ad5dfbafb6c05bbc1118945936a9762
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-13 22:29:50 +0000

    called api

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
 app.use('/static', express.static("public"));
+
+app.get('/', function(req, res){
+    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=d1d7d2ee').then(function(response){
+        Todo.find(function(err, todo){
+            if(err){
+                res.json({"Error: ": err})
+            } else {
+                res.render('todo.ejs', {todoList: todo, comicData: response.data});
+            }
+        })
+    }).catch(function(error){
+        res.json({"Error: ": error})
+    })
+
+})
