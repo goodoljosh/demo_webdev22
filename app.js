7eb7324cd27da3a33ccd9f7f4266f302239622cd
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-14 21:23:11 +0000

    removed get info under axios call

--- app.js
+++ app.js
@@ -5,19 +5,9 @@ app.use('/static', express.static("public"));
 
 app.get('/', function(req, res){
     axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=d1d7d2ee').then(function(response){
-        Todo.find(function(err, todo){
-            if(err){
-                res.json({"Error: ": err})
-            } else {
-                res.render('todo.ejs', {todoList: todo, comicData: response.data});
-            }
-        })
-    }).catch(function(error){
-        res.json({"Error: ": error})
-    })
-
-})
-
+        res.render('apihtml.ejs');
+    }
+    
 app.listen(3000,function(){
     console.log('App listening on port 3000');
 })
