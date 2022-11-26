e49031524c872ad7ea7a91ef9ea57b11d3103c8e
goodoljosh <findley_joshua1@columbusstate.edu>
2022-11-26 22:32:29 +0000

    Made changes to RNG.

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ function randomNumber(min, max) {
 } 
 
 //Random number generator variable
-//3896198, number for Gurdians of the Galaxy
+//3896198, number for Guardians of the Galaxy
 app.get('/', function(req, res){
     //let randomNum=Math.floor(8000000 + Math.random() * 9000000);
     //API call using commented random number generator above
     //axios.get('http://www.omdbapi.com/?i=tt'+randomNum+'&apikey=d1d7d2ee').then(function(response){
-    axios.get('http://www.omdbapi.com/?i=tt'+randomNumber(3896198,4000000)+'&apikey=d1d7d2ee').then(function(response){
+    axios.get('http://www.omdbapi.com/?i=tt'+randomNumber(1000000,3000000)+'&apikey=d1d7d2ee').then(function(response){
         res.render('apihtml.ejs', { data: response.data });
     })
 })
