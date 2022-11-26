ad02012c838eb560368db6f3ac49c3ceb1407257
goodoljosh <findley_joshua1@columbusstate.edu>
2022-11-26 22:01:30 +0000

    Added a minimum and maximum threshold for RNG.

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ var express = require('express');
 var app = express();
 var axios = require('axios');
 app.use('/static', express.static("public"));
+//Genrates a random number based on a minimum and maximum number defined here.
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min) + min);
+} 
 
 //Random number generator variable
 //3896198, number for Gurdians of the Galaxy
-let randomNum=Math.floor(1000000 + Math.random() * 9000000);
 app.get('/', function(req, res){
-    axios.get('http://www.omdbapi.com/?i=tt'+randomNum+'&apikey=d1d7d2ee').then(function(response){
+    //let randomNum=Math.floor(8000000 + Math.random() * 9000000);
+    //API call using commented random number generator above
+    //axios.get('http://www.omdbapi.com/?i=tt'+randomNum+'&apikey=d1d7d2ee').then(function(response){
+    axios.get('http://www.omdbapi.com/?i=tt'+randomNumber(3896198,4000000)+'&apikey=d1d7d2ee').then(function(response){
         res.render('apihtml.ejs', { data: response.data });
     })
 })
-//console.log(Math.floor(1000000 + Math.random() * 9000000));
+
+//console.log(randomNumber(3896198,4000000));
+//Used for testing for random numbers
 app.listen(3000,function(){
     console.log('App listening on port 3000');
 })
